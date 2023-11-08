4eec41c010871fc0cedf3598a7dc415cfbe0fc04
mydongistiny <jaysonedson@gmail.com>
2023-11-09 02:13:27 +0900

    Settings: Sync pixel-framework import
    Co-authored-by: someone5678 <nemui3353@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -197,6 +197,8 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.blackiron.settings.BlackironSettings;
 
+import com.google.android.settings.security.SecurityHubDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -380,7 +382,8 @@ public class SettingsGateway {
             RegionalPreferencesEntriesFragment.class.getName(),
             BatteryInfoFragment.class.getName(),
             UserAspectRatioDetails.class.getName(),
-            BlackironSettings.class.getName()
+            BlackironSettings.class.getName(),
+            SecurityHubDashboard.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

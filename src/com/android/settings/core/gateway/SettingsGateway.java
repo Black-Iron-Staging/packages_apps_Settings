crdroidandroid/android_packages_apps_Settings

Settings: Sync pixel-framework import
Co-authored-by: someone5678 <nemui3353@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -196,6 +196,8 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.crdroid.settings.crDroidSettingsLayout;
 import com.crdroid.settings.fragments.ui.SmartPixels;
 
+import com.google.android.settings.security.SecurityHubDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -379,6 +381,7 @@ public class SettingsGateway {
             crDroidSettingsLayout.class.getName(),
             RunningServices.class.getName(),
             SmartPixels.class.getName(),
+            SecurityHubDashboard.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

Black-Iron-Staging/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -197,6 +197,8 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.blackiron.settings.BlackironSettings;
 
+import com.google.android.settings.security.SecurityHubDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -380,7 +382,8 @@ public class SettingsGateway {
             RegionalPreferencesEntriesFragment.class.getName(),
             BatteryInfoFragment.class.getName(),
             UserAspectRatioDetails.class.getName(),
-            BlackironSettings.class.getName()
+            BlackironSettings.class.getName(),
+            SecurityHubDashboard.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {